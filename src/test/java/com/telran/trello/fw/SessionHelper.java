c70bc6e43d279db884cbc56636ca8180110c06b1
romanichh <58612871+romanichh@users.noreply.github.com>
2020-02-06 00:10:46 +0200

    05.02

--- src/test/java/com/telran/trello/fw/SessionHelper.java
+++ src/test/java/com/telran/trello/fw/SessionHelper.java
@@ -45,9 +45,10 @@ public class SessionHelper extends HelperBase {
         fillLoginForm("romich87", "romanich1987");
     }
 
-    public void logout() {
+    public void logout() throws InterruptedException {
         click(By.cssSelector("[data-test-id='header-member-menu-button']"));
         click(By.cssSelector("[data-test-id='header-member-menu-logout']"));
+        pause(10000);
     }
 
     public void openUserProfileFromDropDown() {
